02e61aeb4f13a555f409579cd5df38089bcb967f
Chris Petty <chris@sussol.net>
2024-03-05 00:10:28 +1300

    4th chunkBuffer!

--- onebrc.go
+++ onebrc.go
@@ -13,7 +13,13 @@ package main
 // 3rd - out of order after much mucking around. Worked out that sending short strings in channel is super slow. []string of 1000 to pool of workers goes to 250+MBps.
 // But, as it turns out what I was doing with a returning channel would create deadlock. So I used go routine with anon function and tried using closures to see what happens
 //// Full file, 320MBps 41s... but the analysis is wrong and different every run LOL. Checking len(measurements) in the routines show it's a mess, ~300 lines rather than 50k
-//// Closures not working how I'd guess from a little side experiment with a counter. Measurements is getting mixed up between threads whoops.
+// Closures not working how I'd guess from a little side experiment with a counter. Measurements is getting mixed up between threads whoops.
+//
+// 4th - goroutines but feeding them with a buffered channel! Outputting by appending to an array captured in the closure appears to be safe.
+//// full file: 45.5s, ~300MBps. Correct output! Curiously measured the aggregation of analysis at the end, only 0.5s. Not worth improving!
+// chunkSize 50k seems to be a sweet spot for ~300MBps. 5k: ~150MBps. 500k+: ~280MBps
+//
+//
 
 import (
 	"bufio"
@@ -39,7 +45,6 @@ func (ca CityAnalysis) toString() string {
 }
 
 func main() {
-	s := time.Now()
 
 	file, err := os.Open("../1brc/measurements.txt")
 	if err != nil {
@@ -49,7 +54,8 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 
-	chunkSize := 50000
+	chunkSize := 500000
+	chunkBuffer := make(chan []string, 10)
 	wg := new(sync.WaitGroup)
 	analysisChunks := make([]map[string]CityAnalysis, 0)
 	measurements := make([]string, 0)
@@ -60,13 +66,16 @@ func main() {
 			measurements = append(measurements, scanner.Text())
 		}
 		if len(measurements) >= chunkSize || !b {
+			chunkBuffer <- measurements
+			measurements = make([]string, 0)
 			wg.Add(1)
 			go func() {
+				chunk := <-chunkBuffer
 				// id := rand.Intn(1000000)
-				// println("routine", id, len(measurements))
+				// println("routine", id, len(chunk))
 				defer wg.Done()
 				analysisChunk := make(map[string]CityAnalysis)
-				for _, line := range measurements {
+				for _, line := range chunk {
 					parts := strings.Split(line, ";") // 180MBps here
 					cityName := parts[0]
 					cityTemp, _ := strconv.ParseFloat(parts[1], 32)
@@ -87,7 +96,6 @@ func main() {
 			if !b {
 				break
 			}
-			measurements = make([]string, 0)
 		}
 	}
 
@@ -98,6 +106,7 @@ func main() {
 	println(len(analysisChunks))
 
 	// Aggregate partial analyses
+	s := time.Now()
 	analysis := make(map[string]CityAnalysis)
 	for _, ca := range analysisChunks {
 		for k, v := range ca {
